bd6198df3c09400fb1ca1f7acc8bda7e8b11bff3
Hidegg <darkvoid214232@gmail.com>
2019-03-28 22:09:39 +0200

    sedinta 6

---
+++ Sedinta04/ex08/app.js
@@ -0,0 +1,10 @@
+var i = 1;
+//Daca i este initializat cu 101 nu va intra in bucla while
+
+while (i <= 100) {
+    console.log(i);
+    if(i === 50){
+        break;
+    }
+    i++
+}
---
+++ Sedinta04/ex10/app.js
@@ -0,0 +1,73 @@
+var person = {
+    name: 'Dragos',
+    surname: 'Iordache',
+    age: 32,
+    petOwner: false,
+    skills: ['html', 'javascript', 'css', 'c++', 'node', 'jquery'],
+    friends:[{
+        name: 'Larry',
+        surname: 'Larryson',
+        age: 30,
+    }, {
+        name: 'Steven',
+        surname: 'Stevenson',
+        age: 31,
+    }, {
+        name: 'Carol',
+        surname: 'Carolson',
+        age: 29,
+    }]
+}
+
+person.skills.forEach(function (skill){
+    console.log(skill);
+});
+
+console.log('----------------------------');
+
+let friendString = 'Prietenii mei se numesc ';
+
+person.friends.forEach(function (friend) {
+        friendString += friend.name + ' ' + friend.surname + ', ';
+});
+
+friendString = friendString.slice(0, -2);
+
+console.log(friendString + '.');
+
+console.log('----------------------------');
+
+let sum = 0;
+
+person.friends.forEach(function (friendage) {
+    sum += friendage.age;
+});
+
+console.log('Suma varstelor este' + sum);
+
+console.log('----------------------------');
+
+let birthYears = 0;
+
+person.friends.forEach(function (friendage) {
+    birthYears += (2019 - friendage.age);
+});
+
+console.log(birthYears);
+
+console.log('----------------------------');
+
+let persAge = person.age;
+
+
+person.friends.forEach(function (friendAge){
+    console.log('Diferenta dintre ' + person.name + ' si ' + friendAge.name + ' este de ' + (persAge - friendAge.age) + '.');
+});
+
+let skillReverse = 0;
+skillReverse = person.skills.reverse;
+
+skillReverse.forEach(function (skill){
+    console.log(skill);
+});
+
