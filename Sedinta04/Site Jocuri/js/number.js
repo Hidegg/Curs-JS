2bf88343d379887f56b7d3f185974e874acd1980
Hidegg <darkvoid214232@gmail.com>
2019-04-09 19:10:45 +0300

    aaa

--- Sedinta04/Site Jocuri/js/number.js
+++ Sedinta04/Site Jocuri/js/number.js
@@ -1,18 +1,19 @@
 var randomNumber = Math.floor(Math.random() * 101),
     userNumber = document.getElementById('number'),
     answer = document.getElementById('message'),
+
     form = document.querySelector('form');
 
 var i = 0,
     maxTries = 0;
 
     
-        form.addEventListener('submit', function(e) {
+        form.addEventListener('submit', function(danieprost) {
             var userNumberValue = parseInt(userNumber.value);
 
-
-            if(userNumberValue < 100 || userNumberValue > 0){
-                if(randomNumber !== userNumberValue){
+            if(userNumberValue > 100 || userNumberValue < 0){
+                answer.innerText = "Numarul trebuie sa fie cuprins intre 1 si 100!";
+            } else if(randomNumber !== userNumberValue){
                     if(randomNumber > userNumberValue){
                         answer.innerText = 'Numarul e prea mic.';
                     }
@@ -20,14 +21,18 @@ var i = 0,
                         answer.innerText = 'Numarul e prea mare.';
                     }
                 }
-            } else {
-                answer.innerText = "Numarul trebuie sa fie cuprins intre 1 si 100!";
-            }
+                
             if(randomNumber === userNumberValue){
                 answer.innerText = 'Ai ghicit! Numarul era ' + userNumberValue + '.';
             }
 
-        e.preventDefault();
+            if(answer !== ' ') {
+                answer.innerText = 'Introdu un numar!';
+            } else {
+                maxTries++;
+            }
+
+        danieprost.preventDefault();
         }, false);
 
         
