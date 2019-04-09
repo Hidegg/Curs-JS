dcf284206eafe3105a69af37c86fc90b444de511
Hideg Alexandru <hidegg@192-168-0-100.rdsnet.ro>
2019-04-09 11:25:32 +0300

    nn

--- Sedinta04/Site Jocuri/js/number.js
+++ Sedinta04/Site Jocuri/js/number.js
@@ -6,30 +6,31 @@ var randomNumber = Math.floor(Math.random() * 101),
 var i = 0,
     maxTries = 0;
 
-    function add(){
-        maxTries += 1;
-    }
     
         form.addEventListener('submit', function(e) {
             var userNumberValue = parseInt(userNumber.value);
 
-            if(randomNumber !== userNumberValue){
-                if(randomNumber > userNumberValue){
-                    answer.innerText = 'Numarul e prea mic.';
+
+            if(userNumberValue < 100 || userNumberValue > 0){
+                if(randomNumber !== userNumberValue){
+                    if(randomNumber > userNumberValue){
+                        answer.innerText = 'Numarul e prea mic.';
+                    }
+                    if(randomNumber < userNumberValue){
+                        answer.innerText = 'Numarul e prea mare.';
+                    }
                 }
-                if(randomNumber < userNumberValue){
-                    answer.innerText = 'Numarul e prea mare.';
-                }
+            } else {
+                answer.innerText = "Numarul trebuie sa fie cuprins intre 1 si 100!";
             }
             if(randomNumber === userNumberValue){
                 answer.innerText = 'Ai ghicit! Numarul era ' + userNumberValue + '.';
             }
 
-            add();
         e.preventDefault();
-        return maxTries += 1;
         }, false);
 
+        
 
 
 //maxTries 10 + dificultate 15/20 tries + button Reset.
